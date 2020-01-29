b60c9ab2b6c5f22091b08d938ca2a22984d87cb8
William Myers <mwilliammyers@gmail.com>
2020-01-29 00:27:22 -0700

    Add chat.sh

--- README.md
+++ README.md
@@ -19,6 +19,5 @@ without docker:
 to run a trained GPT2 model, do:
 
 ```
-source ./venv/bin/activate
-python3 run_gpt2.py --model_name_or_path=/path/to/checkpoints/
+./chat.sh
 ```
---
+++ chat.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+project_dir=$(git rev-parse --show-toplevel 2> /dev/null) 
+
+if [ -n "$project_dir" ]; then
+    py=$(find $project_dir -executable -wholename '*bin/python3*' | head -n 1)
+fi
+
+chkpt_dir=${1:-/mnt/pccfs/not_backed_up/will/checkpoints}
+chkpt_dir=${chkpt_dir:-.}
+
+$py run_gpt2.py \
+    --model_name_or_path="$chkpt_dir/$(ls -1r $chkpt_dir | grep -m 1 conversational-ai)" \
+    $@
+
