f0b7e80cb4c7c08dac6d1b83ee58839308a68e05
William Myers <mwilliammyers@gmail.com>
2020-01-29 18:08:05 -0700

    Add env var dir support

--- docker-train.sh
+++ docker-train.sh
@@ -6,16 +6,16 @@ work_dir=/workspace
 
 local_root_dir=/mnt/pccfs/not_backed_up/will
 
-chkpt_dir=${1:-$local_root_dir/checkpoints/}
+chkpt_dir=${CHCKPOINT_DIR:-$local_root_dir/checkpoints/}
 mkdir -p $chkpt_dir
 
-tb_runs_dir=${2:-$local_root_dir/runs/}
+tb_runs_dir=${TENSORBOARD_RUNS_DIR:-$local_root_dir/runs/}
 mkdir -p $tb_runs_dir
 
 docker pull pccl/$name
 
 # get the ID of the last GPU
-gpu_id=$(nvidia-smi -L | awk 'END { gsub(":", ""); print $2 }')
+gpu_id=${NV_GPU:-$(nvidia-smi -L | awk 'END { gsub(":", ""); print $2 }')}
 
 args="-d --rm --name=$name -v $chkpt_dir/:$work_dir/output/ -v $tb_runs_dir/:$work_dir/runs"
 
