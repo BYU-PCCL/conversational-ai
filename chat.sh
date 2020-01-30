bb90731b53f102e4fe04495315cd05e1d944d99d
William Myers <mwilliammyers@gmail.com>
2020-01-29 18:03:14 -0700

    Pass through args to run_gpt2.py

--- chat.sh
+++ chat.sh
@@ -6,10 +6,9 @@ if [ -n "$project_dir" ]; then
     py=$(find $project_dir -executable -wholename '*bin/python3*' | head -n 1)
 fi
 
-chkpt_dir=${1:-/mnt/pccfs/not_backed_up/will/checkpoints}
-chkpt_dir=${chkpt_dir:-.}
+chkpt_dir=${CHECKPOINT_DIR:-/mnt/pccfs/not_backed_up/will/checkpoints}
 
 $py run_gpt2.py \
     --model_name_or_path="$chkpt_dir/$(ls -1r $chkpt_dir | grep -m 1 conversational-ai)" \
-    --length=20
+    "$@"
 
