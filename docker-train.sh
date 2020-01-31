e451c027227421b1cc7574ae8fa47f39277b22cb
William Myers <mwilliammyers@gmail.com>
2020-01-30 19:08:17 -0700

    Update docker args

--- docker-train.sh
+++ docker-train.sh
@@ -18,7 +18,7 @@ docker pull pccl/$docker_image
 # get the ID of the last GPU
 gpu_id=${NV_GPU:-$(nvidia-smi -L | awk 'END { gsub(":", ""); print $2 }')}
 
-args="-d --rm --name=$run_name -e TENSORBOARD_RUN=$run_name -v $chkpt_dir/:$work_dir/checkpoints/ -v $tb_runs_dir/:$work_dir/runs"
+args="-d --rm --name=$run_name --ipc=host -e TENSORBOARD_RUN=$run_name -v $chkpt_dir/:$work_dir/checkpoints/ -v $tb_runs_dir/:$work_dir/runs"
 
 if [ -x "$(command -v nvidia-docker)" ]; then
     NV_GPU=$gpu_id nvidia-docker run $args pccl/$docker_image $@
