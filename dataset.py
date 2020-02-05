61bdb0d45e849bae4ec0a32c540d3cd2ef90db11
William Myers <mwilliammyers@gmail.com>
2020-02-04 19:59:28 -0700

    Fix dataset index of human participant

--- dataset.py
+++ dataset.py
@@ -11,7 +11,7 @@ def _prep_convo(
     end_token="<|endoftext|>",
     # max_utterance_length=int(sys.argv[1]) if len(sys.argv) > 1 else 0,
 ):
-    convo = ["> " + txt if i % 2 != 0 else txt for i, txt in enumerate(convo)]
+    convo = ["> " + txt if i % 2 == 0 else txt for i, txt in enumerate(convo)]
     return "{}\n{}\n{}".format(start_token, "\n".join(convo), end_token)
 
 
@@ -31,3 +31,9 @@ def write_to_file(path="train.txt", daily_dialog_path="dialogues_text.txt"):
     Path(path).write_text("\n".join(dataset))
 
     return path
+
+
+if __name__ == "__main__":
+    import os
+
+    write_to_file(os.getenv("TRAIN_FILE", "train.txt"))
