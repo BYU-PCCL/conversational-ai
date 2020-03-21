503f98cd7f0c538c47bb673f2f10e08e8f2fa1d7
William Myers <mwilliammyers@gmail.com>
2020-03-21 00:04:10 -0600

    Add validation_path to dataset

--- dataset.py
+++ dataset.py
@@ -3,11 +3,15 @@
 https://github.com/google-research/text-to-text-transfer-transformer
 """
 
+import os
+import random
 import re
 from pathlib import Path
-from typing import Iterable, List, Optional, Union
+from typing import Iterable, List, Optional, Tuple, Union
 
 import chitchat_dataset as ccc
+
+_Path = Union[str, Path]
 
 
 def _prep_convo(convo: Iterable[str]) -> List[str]:
@@ -26,11 +30,13 @@ def _prep_convo(convo: Iterable[str]) -> List[str]:
     return result
 
 
-def write_to_file(
-    path: Union[str, Path] = "train.tsv",
-    daily_dialog_path: Optional[Union[str, Path]] = None,
-) -> Union[str, Path]:
-    """Writes the CCC (and optionally the daily dialog) dataset to a file."""
+def write_to_files(
+    train_path: _Path = "train.tsv",
+    validation_path: _Path = "validation.tsv",
+    daily_dialog_path: Optional[_Path] = os.getenv("DAILY_DIALOG_PATH"),  # noqa: B008
+    train_ratio: float = 0.9,
+) -> Tuple[_Path, _Path]:
+    """Write the CCC (& optionally daily dialog) dataset(s) to train/validation files."""
     dataset = []
 
     if daily_dialog_path:
@@ -41,12 +47,20 @@ def write_to_file(
         convo = _prep_convo(" ".join(u["text"] for u in m) for m in convo["messages"])
         dataset.extend(convo)
 
-    Path(path).write_text("\n".join(dataset))  # type: ignore
+    random.shuffle(dataset)
 
-    return path
+    train_idx = int(train_ratio * len(dataset))
+    Path(train_path).write_text("\n".join(dataset[:train_idx]))  # type: ignore
+    Path(validation_path).write_text("\n".join(dataset[train_idx:]))  # type: ignore
+
+    return train_path, validation_path
 
 
 if __name__ == "__main__":
     import os
 
-    write_to_file(os.getenv("TRAIN_FILE", "train.tsv"), os.getenv("DAILY_DIALOG_PATH"))
+    write_to_files(
+        os.getenv("CONVERSATIONAL_AI_TRAIN_PATH", "train.tsv"),
+        os.getenv("CONVERSATIONAL_AI_VALIDATION_PATH", "validation.tsv"),
+        daily_dialog_path=os.getenv("DAILY_DIALOG_PATH"),
+    )
