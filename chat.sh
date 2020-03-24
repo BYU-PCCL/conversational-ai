d65353da6f471f8dd2137043729fae17cd2b10ce
William Myers <mwilliammyers@gmail.com>
2020-03-24 00:40:28 -0600

    Fix or ignore shellcheck issues

--- chat.sh
+++ chat.sh
@@ -2,7 +2,7 @@
 
 docker run \
     -it --rm --ipc=host --gpus=all \
-    -v ${CONVERSATIONAL_AI_CHECKPOINT_DIR:-$(git rev-parse --show-toplevel || $PWD)/checkpoint}:/checkpoint/ \
-    -v $PWD/chats:/chats/ \
+    -v "${CONVERSATIONAL_AI_CHECKPOINT_DIR:-$(git rev-parse --show-toplevel || $PWD)/checkpoint}:/checkpoint/" \
+    -v "$PWD/chats:/chats/" \
     pccl/conversational-ai \
-    python3 chat.py $@
+    python3 chat.py "$@"
--- serve.sh
+++ serve.sh
@@ -11,6 +11,7 @@ gpu_id=${NV_GPU:-all}
 run_name=${docker_image_name}_server
 
 chkpt_dir=${CONVERSATIONAL_AI_CHECKPOINT_DIR:-/mnt/pccfs/not_backed_up/will/checkpoints}
+# shellcheck disable=SC2086,SC2012
 chkpt=${CONVERSATIONAL_AI_CHECKPOINT:-$chkpt_dir/$(ls -1r $chkpt_dir | head -n 1)}
 
 # chats_dir=${CONVERSATIONAL_AI_CHATS_DIR:-/mnt/pccfs/backed_up/will/chats}
@@ -19,9 +20,11 @@ args=$(env | awk 'BEGIN { ORS=" " }; $0 ~ /CONVERSATIONAL_AI_/ { print "-e", $0 
 args="-d --rm --name=$run_name -p 8080:8080 --ipc=host -v $chkpt/:/checkpoint/ $args"
 
 if [ -x "$(command -v nvidia-docker)" ]; then
+    # shellcheck disable=SC2086
     NV_GPU=$gpu_id nvidia-docker run $args pccl/$docker_image_name python3 serve.py
 else
-    docker run --gpus=$gpu_id $args pccl/$docker_image_name python3 serve.py
+    # shellcheck disable=SC2086
+    docker run --gpus="$gpu_id" $args pccl/$docker_image_name python3 serve.py
 fi
 
 echo ""
