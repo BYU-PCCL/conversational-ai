b6f8616d5d7df98d9bed78a96122d645a0741ce1
William Myers <mwilliammyers@gmail.com>
2020-03-24 02:28:16 -0600

    Remove unused files
    We will need to add their functionality back sometime soon.

--- chat.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker run \
-    -it --rm --ipc=host --gpus=all \
-    -v "${CONVERSATIONAL_AI_CHECKPOINT_DIR:-$(git rev-parse --show-toplevel || $PWD)/checkpoint}:/checkpoint/" \
-    -v "$PWD/chats:/chats/" \
-    pccl/conversational-ai \
-    python3 chat.py "$@"
