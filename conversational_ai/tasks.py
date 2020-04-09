4acc2cab5ec36369fc9c232bfaaa6872b62ade29
William Myers <mwilliammyers@gmail.com>
2020-04-08 21:26:55 -0600

    Add nsp task
    Also refactor tasks

--- conversational_ai/tasks.py
+++ conversational_ai/tasks.py
@@ -3,42 +3,66 @@
 https://github.com/google-research/text-to-text-transfer-transformer
 """
 import functools
-from typing import Dict, Iterable
+from typing import Callable, Dict, Iterable, Iterator
 
 import chitchat_dataset as ccc
 import t5
 import tensorflow.compat.v1 as tf
 
 
-def _compounding_dataset(num: int, split: str, shuffle_files: bool) -> tf.data.Dataset:
-    """Creates a tf.data.Dataset of input/target examples.
+def _generate_compounding_conversations() -> Iterable[Dict[str, str]]:
+    for inputs, targets in ccc.CompoundingConversationDataset(prefix="converse: "):
+        yield {"inputs": inputs, "targets": targets}
 
-    Each example has the form: `{"inputs": tf.string, "targets": tf.string}`.
-    """
 
-    def _generate_dataset() -> Iterable[Dict[str, str]]:
-        for inputs, targets in ccc.CompoundingConversationDataset(prefix="converse: "):
-            yield {"inputs": inputs, "targets": targets}
-
+def _dataset(
+    split: str,
+    shuffle_files: bool,
+    generator: Callable[[], Iterable[Dict[str, str]]],
+    keys: Iterator[str],
+    num_train: int,
+) -> tf.data.Dataset:
     dataset = tf.data.Dataset.from_generator(
-        _generate_dataset,
-        output_types={"inputs": tf.string, "targets": tf.string},
-        output_shapes={"inputs": tf.TensorShape([]), "targets": tf.TensorShape([])},
+        generator,
+        output_types={k: tf.string for k in keys},
+        output_shapes={k: tf.TensorShape([]) for k in keys},
     )
-    return dataset.take(num) if split == "train" else dataset.skip(num)
+    return dataset.take(num_train) if split == "train" else dataset.skip(num_train)
 
 
 t5.data.TaskRegistry.add(
     "conversation_v001_compounding",
     t5.data.Task,
-    dataset_fn=functools.partial(_compounding_dataset, 124_990),
+    dataset_fn=functools.partial(
+        _dataset,
+        generator=_generate_compounding_conversations,
+        keys=["inputs", "targets"],
+        num_train=124_990,
+    ),
     splits=["train", "validation"],
     text_preprocessor=None,
     postprocess_fn=t5.data.postprocessors.lower_text,
     metric_fns=[
         t5.evaluation.metrics.accuracy,
-        t5.evaluation.metrics.rouge,
         t5.evaluation.metrics.bleu,
+        t5.evaluation.metrics.rouge,
     ],
+    sentencepiece_model_path=t5.data.DEFAULT_SPM_PATH,
+)
+
+
+t5.data.TaskRegistry.add(
+    "conversation_v001_nsp",
+    t5.data.Task,
+    dataset_fn=functools.partial(
+        _dataset,
+        generator=lambda: ({"text": "\n".join(c)} for c in ccc.ConversationDataset()),
+        keys=["text"],
+        num_train=6952,
+    ),
+    splits=["train", "validation"],
+    text_preprocessor=t5.data.preprocessors.next_sentence_prediction,
+    postprocess_fn=t5.data.postprocessors.lower_text,
+    metric_fns=[t5.evaluation.metrics.accuracy],
     sentencepiece_model_path=t5.data.DEFAULT_SPM_PATH,
 )
